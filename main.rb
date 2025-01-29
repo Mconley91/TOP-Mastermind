2cb7bf81c91ef4b5a9e5734d8a001ffe9e4acabe
mconley91 <matthew.e.conley@gmail.com>
2025-01-29 16:08:56 -0500

    add feedback method, fix check_for_winner method game is now fully functional

--- main.rb
+++ main.rb
@@ -3,16 +3,16 @@
 # each round the amount of correctly placed pins should be displayed along with the amount of incorrect pins
 # 12 total rounds
 class Game
-  attr_accessor(:arr, :turn, :feedback)
+  attr_accessor(:arr, :turn, :correct_entires)
 
   def initialize(arr)
     @turn = 1
     @arr = arr
-    @feedback = 0
+    @correct_entires = 0
   end
 
   def draw_board
-    12.times { |i| puts "#{i} #{arr[i]}" }
+    12.times { |i| puts "#{i + 1} #{arr[i]}" }
   end
 
   def increment_turn
@@ -28,6 +28,19 @@ class Game
     end
     code
   end
+
+  def check_for_winner(input, code)
+    input.split(' ') == code
+  end
+
+  def update_board(input, turn)
+    arr[turn - 1] = input.split(' ') << "#{correct_entires} correct entries"
+  end
+
+  def feedback(input, code)
+    self.correct_entires = 0
+    input.split(' ').each_with_index { |entry, index| entry == code[index] ? self.correct_entires += 1 : '' }
+  end
 end
 
 current_game = Game.new([%w[_ _ _ _], %w[_ _ _ _], %w[_ _ _ _], %w[_ _ _ _],
@@ -37,12 +50,19 @@ current_game = Game.new([%w[_ _ _ _], %w[_ _ _ _], %w[_ _ _ _], %w[_ _ _ _],
 def playing(current_game)
   code = current_game.code_picker
   current_game.draw_board
+  puts "input 'A B C D' in the correct order to crack the code!"
   while current_game.turn <= 12
-    puts "Testing code picker code: #{code}"
     puts "Current Turn: #{current_game.turn}"
-    puts "input 'A B C D' in the correct order to break the code!"
     input = gets.chomp
-    current_game.increment_turn
+    current_game.feedback(input, code)
+    current_game.update_board(input, current_game.turn)
+    current_game.draw_board
+    if current_game.check_for_winner(input, code)
+      puts 'You won: You cracked the code!'
+      return
+    else
+      current_game.increment_turn
+    end
   end
   puts 'Game Over: Turn limit reached!'
 end
