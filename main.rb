a723ec2b819263a7d1501f55a6062f6e186ea4e2
mconley91 <matthew.e.conley@gmail.com>
2025-01-29 21:48:44 -0500

    add validate_input and hanlde_turn methods for class to declutter old playing method

--- main.rb
+++ main.rb
@@ -1,14 +1,17 @@
+# frozen_string_literal: true
+
 # game should print a depiction of the mastermind board game on each round
 # round should continue until the player wins or runs out of attempts
 # each round the amount of correctly placed pins should be displayed along with the amount of incorrect pins
 # 12 total rounds
 class Game
-  attr_accessor(:arr, :turn, :correct_entires)
+  attr_accessor(:arr, :turn, :correct_entires, :code)
 
   def initialize(arr)
     @turn = 1
     @arr = arr
     @correct_entires = 0
+    @code = []
   end
 
   def draw_board
@@ -21,7 +24,6 @@ class Game
 
   def code_picker
     code_options = %w[A B C D]
-    code = []
     while code.length < 4
       cpu_choice = code_options[rand(4)]
       code << cpu_choice unless code.any? { |ele| ele == cpu_choice }
@@ -41,35 +43,42 @@ class Game
     self.correct_entires = 0
     input.split(' ').each_with_index { |entry, index| entry == code[index] ? self.correct_entires += 1 : '' }
   end
+
+  def validate_input(input)
+    input.split(' ').all? { |entry| code.include?(entry) }
+  end
+
+  def handle_turn
+    while turn <= 12
+      puts "Current Turn: #{turn}"
+      input = gets.chomp
+      if validate_input(input)
+        feedback(input, code)
+        update_board(input, turn)
+        draw_board
+        if check_for_winner(input, code)
+          puts 'You Won: You cracked the code!'
+          return
+        else
+          increment_turn
+        end
+      else
+        puts 'Invalid Entry!'
+      end
+    end
+    puts 'Game Over: Turn limit reached!'
+  end
 end
 
 current_game = Game.new([%w[_ _ _ _], %w[_ _ _ _], %w[_ _ _ _], %w[_ _ _ _],
                          %w[_ _ _ _], %w[_ _ _ _], %w[_ _ _ _], %w[_ _ _ _],
                          %w[_ _ _ _], %w[_ _ _ _], %w[_ _ _ _], %w[_ _ _ _]])
 
-def playing(current_game)
-  code = current_game.code_picker
+def start_game(current_game)
+  current_game.code_picker
   current_game.draw_board
-  puts "input 'A B C D' in the correct order to crack the code!"
-  while current_game.turn <= 12
-    puts "Current Turn: #{current_game.turn}"
-    input = gets.chomp
-    if input.split(' ').all? { |entry| code.include?(entry) }
-      current_game.feedback(input, code)
-      current_game.update_board(input, current_game.turn)
-      current_game.draw_board
-      if current_game.check_for_winner(input, code)
-        puts 'You Won: You cracked the code!'
-        return
-      else
-        current_game.increment_turn
-      end
-    else
-      puts 'Invalid Input!'
-    end
-
-  end
-  puts 'Game Over: Turn limit reached!'
+  puts "input 'A B C D' in the correct order to crack the code! You have 12 Turns."
+  current_game.handle_turn
 end
 
-playing(current_game)
+start_game(current_game)
