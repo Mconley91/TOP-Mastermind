f8445d0b613e9a69d611c71a7a0a1cfaf23c7050
mconley91 <matthew.e.conley@gmail.com>
2025-01-30 19:57:02 -0500

    Mastermind gamemode now fully functional

--- main.rb
+++ main.rb
@@ -5,21 +5,30 @@
 # each round the amount of correctly placed pins should be displayed along with the amount of incorrect pins
 # 12 total rounds
 class Game
-  attr_accessor(:arr, :turn, :correct_entires, :code, :code_options, :user_input)
+  attr_accessor(:arr, :turn, :correct_entires, :mastermind_code, :code_options, :codebreaker_input)
 
   def initialize(arr)
     @turn = 1
     @arr = arr
     @correct_entires = 0
-    @code = []
+    @mastermind_code = []
     @code_options = %w[A B C D]
-    @user_input = []
+    @codebreaker_input = []
   end
 
-  def code_picker
-    while code.length < 4
+  def generate_mastermind_code
+    self.mastermind_code = []
+    while mastermind_code.length < 4
       cpu_choice = code_options[rand(4)]
-      code << cpu_choice unless code.any? { |ele| ele == cpu_choice }
+      mastermind_code << cpu_choice unless mastermind_code.any? { |ele| ele == cpu_choice }
+    end
+  end
+
+  def generate_codebreaker_input
+    self.codebreaker_input = []
+    while codebreaker_input.length < 4
+      cpu_choice = code_options[rand(4)]
+      codebreaker_input << cpu_choice unless codebreaker_input.any? { |ele| ele == cpu_choice }
     end
   end
 
@@ -28,7 +37,7 @@ class Game
   end
 
   def validate_input
-    user_input.all? { |entry| code_options.include?(entry) }
+    codebreaker_input.all? { |entry| code_options.include?(entry) }
   end
 
   def increment_turn
@@ -37,14 +46,17 @@ class Game
 
   def update_board
     self.correct_entires = 0
-    user_input.each_with_index { |entry, index| entry == code[index] ? self.correct_entires += 1 : '' }
-    arr[turn - 1] = user_input + ["#{correct_entires} correct entries"]
+    codebreaker_input.each_with_index do |entry, index|
+      entry == mastermind_code[index] ? self.correct_entires += 1 : ''
+    end
+    arr[turn - 1] = codebreaker_input + ["#{correct_entires} correct entries"]
     draw_board
   end
 
   def handle_codebreaker_game
+    generate_mastermind_code
     while turn <= 12
-      self.user_input = gets.chomp.split(' ')
+      self.codebreaker_input = gets.chomp.split(' ')
       if validate_input
         update_board
         return if check_for_winner
@@ -52,31 +64,29 @@ class Game
         puts 'Invalid Entry!'
       end
     end
-    puts "Game Over: Turn limit reached! Mastermind wins! The code was #{code}."
+    puts "Game Over: Turn limit reached! Mastermind wins! The code was #{mastermind_code}."
   end
 
   def handle_mastermind_game
-    self.code = gets.chomp.split(' ')
+    self.mastermind_code = gets.chomp.split(' ')
     if validate_input
       while turn <= 12
-        self.code = []
-        code_picker
+        generate_codebreaker_input
         update_board
         return if check_for_winner
       end
-      puts "Game Over: Turn limit reached! Mastermind wins! The code was #{code}."
+      puts "Game Over: Turn limit reached! Mastermind wins! The code was #{mastermind_code}."
     else
       puts 'Invalid Entry!'
     end
   end
 
   def check_for_winner
-    if user_input == code
-      puts 'You Won: Codebreaker cracked the code!'
+    if codebreaker_input == mastermind_code
+      puts "Game Over!: Codebreaker cracked the code! The code was #{mastermind_code}."
       true
     else
-      puts "#{user_input} #{code}"
-      puts "#{user_input == code}"
+      # puts "Codebreaker's Input: #{codebreaker_input}  Mastermind's Code: #{mastermind_code}"  # for troubleshooting
       increment_turn
       puts "Current Turn: #{turn}"
       false
@@ -88,11 +98,11 @@ def start_game(current_game)
   playing = false
   until playing
     puts "Enter 'Codebreaker' to crack a code or 'Mastermind' to make a code the computer will try to crack."
-    input = gets.chomp
-    if input == 'Codebreaker'
+    input = gets.chomp.downcase
+    if input == 'codebreaker'
       playing = true
       codebreaker_game(current_game)
-    elsif input == 'Mastermind'
+    elsif input == 'mastermind'
       playing = true
       mastermind_game(current_game)
     else
@@ -102,7 +112,6 @@ def start_game(current_game)
 end
 
 def codebreaker_game(current_game)
-  current_game.code_picker
   current_game.draw_board
   puts "input 'A B C D' in the correct order to crack the code! You have 12 Turns."
   current_game.handle_codebreaker_game
